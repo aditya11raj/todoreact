68e47e13398d99bcd16de7a0e9a4482f3fefd9ce
codemonger11 <adityaiiitl@gmail.com>
2021-03-15 19:12:16 +0530

    todomapped

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react"
 import TodoItem from "./components/todoitem"
+import todoData from "./components/tododata"
 import "./style.css"
 function App() {
+    const todoItems=todoData.map(item => <TodoItem key={item.id} item={item}/>)
     return (
         <div className="todo-list">
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+            {todoItems}
         </div>
     )
 }
